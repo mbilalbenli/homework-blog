5b32aa029133d409d78c8c29920e2b59c7092f67
M. Bilal Benli <180403075@ogr.gelisim.edu.tr>
2022-05-13 13:31:49 +0300

    new packages and delet put methotds

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {
+mongoose.connect('mongodb://127.0.0.1:27017/blog', {
   useNewUrlParser: true, useUnifiedTopology: true, 
 })
 
@@ -11,13 +13,10 @@ app.set('view engine', 'ejs')
 
 app.use('/articles', articleRouter)
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
-    const articles = [{
-        title : "test article",
-        createdAt : Date.now(),
-        description : "this is a test article"
-    }]
+    const articles = await Article.find().sort({ createdAt: 'desc' })
     res.render('articles/index', {articles: articles})
 })
 
