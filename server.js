626d070925054ca11e0cbaf432507b9b8a9feb9b
M. Bilal Benli <180403075@ogr.gelisim.edu.tr>
2022-05-13 10:43:38 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
+app.get('/', async (req, res) => {
+    res.render('index')
+})
+
+app.listen(5000) 
